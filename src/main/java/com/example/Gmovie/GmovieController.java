0eba6e3125c2db56b64ab67dfa5fc1aec8669954
varadharajanthiagarajan <varadha.001@gmail.com>
2021-03-20 11:51:43 -0400

    Commit on Green  - implemented post method , also correct the method name for the getmovie inside the controller

--- src/main/java/com/example/Gmovie/GmovieController.java
+++ src/main/java/com/example/Gmovie/GmovieController.java
@@ -1,14 +1,22 @@
 package com.example.Gmovie;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GmovieController {
 
     @GetMapping("/movies")
-    public String getBooks(){
+    public String getMovies(){
         return "[]";
     }
 
+    @PostMapping("/movies")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addMovie(){
+
+    }
 }
