55f2132758030ae5a96562c2676f8856cca1c069
varadharajanthiagarajan <varadha.001@gmail.com>
2021-03-20 11:02:08 -0400

    initial commit

---
+++ src/test/java/com/example/Gmovie/GmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Gmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
